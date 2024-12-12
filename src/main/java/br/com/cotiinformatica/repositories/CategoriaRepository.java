18c6f05c6546b82b753eb43a88bf17237b0299e6
jorgevip <jmonteiro.consultorvip@gmail.com>
2024-12-12 20:48:26 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/repositories/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package br.com.cotiinformatica.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cotiinformatica.entities.Categoria;
+
+@Repository
+public interface CategoriaRepository 
+  extends JpaRepository<Categoria, UUID> {
+
+}
